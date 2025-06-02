1ce992af29f919c1074f16e3180f17d4a1baf2d2
meet-moradiya <mdmoradiya2705@gmail.com>
2025-06-03 00:55:05 +0530

    footer added in layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,8 @@ import CurrentPage from "@/components/CurrentPage";
 import BackToTop from "@/components/BackToTop";
 import CustomScrollbar from "@/components/CustomScrollbar";
 import Navbar from "@/components/Navbar";
-<<<<<<< HEAD
 import LoadingProvider from "@/components/LoadingProvider";
-=======
 import Footer from "@/components/Footer";
->>>>>>> 0f41d5e (navbar's hamburger line fixed and footer added)
 
 export const metadata = {
     title: "Meet Moradiya",
@@ -25,7 +22,6 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-<<<<<<< HEAD
                 <LoadingProvider>
                     <CustomCursor />
                     <CustomScrollbar />
@@ -34,23 +30,12 @@ export default function RootLayout({ children }) {
                         <CurrentPage />
                         <BackToTop />
                         <Navbar />
-                        <div id="smooth-content">{children}</div>
+                        <div id="smooth-content">
+                            {children}
+                            <Footer />
+                        </div>
                     </div>
                 </LoadingProvider>
-=======
-                <CustomCursor />
-                <CustomScrollbar />
-                <ScrollTriggerInit />
-                <div id="smooth-wrapper">
-                    <CurrentPage />
-                    <BackToTop />
-                    <Navbar />
-                    <div id="smooth-content">
-                        {children}
-                        <Footer />
-                    </div>
-                </div>
->>>>>>> 0f41d5e (navbar's hamburger line fixed and footer added)
             </body>
         </html>
     );
